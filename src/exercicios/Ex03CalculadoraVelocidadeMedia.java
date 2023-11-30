6beca32235c684650e48046a32645de4546def17
joaoCesarV <joaocesarvalencadesantana@gmail.com>
2023-11-30 08:24:43 -0300

    commit

---
+++ src/exercicios/Ex03CalculadoraVelocidadeMedia.java
@@ -0,0 +1,20 @@
+package exercicios;
+import java.util.Scanner;
+
+public class Ex03CalculadoraVelocidadeMedia {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Insira a distância percorrida pelo carro em km:");
+        double distancia = input.nextDouble();
+
+        System.out.println("Insira o tempo gasto em horas:");
+        double tempo = input.nextDouble();
+
+        double velocidadeMedia = distancia / tempo;
+
+        System.out.println("A velocidade média do carro é: " + velocidadeMedia + " km/h");
+
+    }
+}
---
+++ src/exercicios/Ex04CalculadoraSalario.java
@@ -0,0 +1,20 @@
+package exercicios;
+import java.util.Scanner;
+
+public class Ex04CalculadoraSalario {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Insira o número de horas trabalhadas:");
+        double horasTrabalhadas = input.nextDouble();
+
+        System.out.println("Insira o valor da hora de trabalho:");
+        double valorHora = input.nextDouble();
+
+        double salarioBruto = horasTrabalhadas * valorHora;
+
+        System.out.println("O salário bruto do funcionário é: " + salarioBruto + " R$");
+
+    }
+}
