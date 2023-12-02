21315fae8c572be31c42f4e64f8bdab610bf17af
joaoCesarV <joaocesarvalencadesantana@gmail.com>
2023-12-02 08:39:23 -0300

    Update Ex007CalculadoraFrete.java

--- src/exercicios/Ex007CalculadoraFrete.java
+++ src/exercicios/Ex007CalculadoraFrete.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class Ex007CalculadoraFrete {
 
+	//Crie um programa que leia o peso de uma encomenda (em kg) e imprima o valor do frete (considerando que cada kg custa R$5,00).
+
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
