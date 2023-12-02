7ef3695bcbe854aece0f417fdb44a4ea9b6eb837
joaoCesarV <joaocesarvalencadesantana@gmail.com>
2023-12-02 08:37:41 -0300

    Update Ex004CalculadoraSalario.java

--- src/exercicios/Ex004CalculadoraSalario.java
+++ src/exercicios/Ex004CalculadoraSalario.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class Ex004CalculadoraSalario {
 
+    \\ Crie um programa que leia o número de horas trabalhadas por um funcionário e o valor da hora de trabalho e imprima o salário bruto desse funcionário.
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
